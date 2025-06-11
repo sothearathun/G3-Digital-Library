3e1bb8bb347444347172de8a4a8bd07605ea87b4
Kleng09 <156322817+Kleng09@users.noreply.github.com>
2025-06-12 00:00:07 +0700

    Add files via upload

---
+++ resources/views/Header_Footer/header.blade.php
@@ -0,0 +1,12 @@
+        
+        <header>
+            <a href="#" class="logo">DIGITALES</a>
+            <nav>
+                <ul>
+                    <li><a href="#">about</a></li>
+                    <li><a href="#">browse</a></li>
+                    <li><a href="#">mode</a></li>
+                </ul>
+            </nav>
+            <div class="profile-icon">👤</div> 
+        </header>
