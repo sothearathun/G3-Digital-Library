bad092f8f9dd850f3ccdbdb15f219d7d414a0c93
kakadaaaaaaaaa <khuot@paragoniu.edu.kh>
2025-06-15 10:30:21 +0700

    try updating web routes

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
 // http://127.0.0.1:8000/index
 Route::get('/index', [Library_controller::class, 'index']);
 
-
+// for search bar
 // for search bar
 // http://127.0.0.1:8000/search
 Route::get('/search', [Library_controller::class, 'search'])->name('books.search');
