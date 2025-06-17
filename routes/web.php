747c088761b31811f46cfb220cd1890cc0295655
Kleng09 <156322817+Kleng09@users.noreply.github.com>
2025-06-17 16:54:10 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,14 @@ use App\Http\Controllers\Library_controller;
 
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 // http://127.0.0.1:8000/index
-Route::get('/index', [Library_controller::class, 'index']);
+Route::get('/index', [Library_controller::class, 'homepage']);
 
-// for search bar
+
 // for search bar
 // http://127.0.0.1:8000/search
 Route::get('/search', [Library_controller::class, 'search'])->name('books.search');
