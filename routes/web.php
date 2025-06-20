8daef15e402621ae04bcfd6fc2c442794eca3868
Kleng09 <156322817+Kleng09@users.noreply.github.com>
2025-06-20 16:16:45 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Web\Library_controller;
+use App\Http\Controllers\Library_controller;
 
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-// http://127.0.0.1:8000/index
+// http://127.0.0.1:8000/homepage
 Route::get('/homepage', [Library_controller::class, 'homepage']);
 
 
 // for search bar
 // http://127.0.0.1:8000/search
 Route::get('/search', [Library_controller::class, 'search'])->name('books.search');
+
+
+// http://127.0.0.1:8000/profile
+Route::get('/profile', [Library_controller::class, 'profile']);
+
+// http://127.0.0.1:8000/search_page
+Route::get('/search_page', [Library_controller::class, 'search_page']);
+
+
+
+
+    
+
+
+
+
+
+
