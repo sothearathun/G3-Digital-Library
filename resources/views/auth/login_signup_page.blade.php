8e3120d1455e5abdfb122bc654effe612c95b00f
Kleng09 <156322817+Kleng09@users.noreply.github.com>
2025-06-25 15:42:00 +0700

    Add files via upload

---
+++ resources/views/auth/login_signup_page.blade.php
@@ -0,0 +1,27 @@
+
+
+<!-- 
+i saw in git that u split every parts like: loginm signup, forget ps 
+but i already conbined everything..... but feel free to split it apart
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+
+
+    <!-- Header page
+    <x-navigation.header/> -->
+
+    <!-- login page -->
+    <x-login_signup.log_sign/>
+
+    <!-- footer page
+    <x-navigation.footer/> -->
+</body>
+</html>
