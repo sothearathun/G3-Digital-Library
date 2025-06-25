1db3225f0973a73d8ff61c386f056745979b034e
Kleng09 <156322817+Kleng09@users.noreply.github.com>
2025-06-25 15:37:11 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\Library_controller;
 
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// http://127.0.0.1:8000
+Route::get('/', [Library_controller::class, 'login_signup_page']);
 
 // http://127.0.0.1:8000/homepage
-Route::get('/homepage', [Library_controller::class, 'homepage'])->name('homepage');
+Route::get('/homepage', [Library_controller::class, 'homepage']);
 
 
-// for search bar
+// for search bar, idk whether u will need it or not (delete it if u dont)
 // http://127.0.0.1:8000/search
 Route::get('/search', [Library_controller::class, 'search'])->name('books.search');
 
@@ -22,18 +20,18 @@ Route::get('/search', [Library_controller::class, 'search'])->name('books.search
 Route::get('/profile', [Library_controller::class, 'profile']);
 
 // http://127.0.0.1:8000/search_page
-Route::get('/search_page', [Library_controller::class, 'search_page'])->name('search_page');
+Route::get('/search_page', [Library_controller::class, 'search_page']);
 
 
-// http://127.0.0.1:8000/viewbook/{book_id}
-Route::get('/viewbook/{book_id}', [Library_controller::class, 'viewbook'])->name('viewbook');
-// http://127.0.0.1:8000/readbook/{book_id}
-Route::get('/readbook/{book_id}', [Library_controller::class, 'readbook'])->name('readbook');
-// http://127.0.0.1:8000/readbook/{book_id}
-Route::match(['get', 'post'], '/storeReadingProgress', [Library_controller::class, 'storeReadingProgress'])->name('storeReadingProgress');
+// http://127.0.0.1:8000/viewbook
+Route::get('/viewbook', [Library_controller::class, 'viewbook']);
+// accessing the page directly dont work idk why,         
+// but u can access it by clicking on any book on 
+// 'homepage' -> any book under'Trending book'
+
 
 // http://127.0.0.1:8000/aboutus
-Route::get('/aboutus', [Library_controller::class, 'aboutus'])->name('aboutus');
+Route::get('/aboutus', [Library_controller::class, 'aboutus']);
 
 // http://127.0.0.1:8000/faq
 Route::get('/faq', [Library_controller::class, 'faq']);
@@ -41,8 +39,6 @@ Route::get('/faq', [Library_controller::class, 'faq']);
 // http://127.0.0.1:8000/terms
 Route::get('/terms', [Library_controller::class, 'terms_conditions']);
 
-
-
-
-
+// http://127.0.0.1:8000/pick_genre
+Route::get('/pick_genre', [Library_controller::class, 'pick_genre']);
 
