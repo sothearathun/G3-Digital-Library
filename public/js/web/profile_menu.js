f55dacc1051e69d68a7a7caa1b9fc249e1edc34c
sothearathun <thunsotheara01@gmail.com>
2025-06-29 19:55:31 +0700

    make this admin only

--- public/js/web/profile_menu.js
+++
@@ -1,6 +0,0 @@
-const profileIcon = document.querySelector('.profile-icon');
-const profileMenu = document.querySelector('.menu');
-
-profileIcon.addEventListener('click', function() {
-    profileMenu.style.display = profileMenu.style.display === 'block' ? 'none' : 'block';
-});
--- resources/views/components/home/popular-author.blade.php
+++
@@ -1,9 +0,0 @@
-{{-- resources/views/components/popular-authors.blade.php --}}
-
-@foreach ($authors as $author)
-<div class="author-card">
-    <!-- <img src="{{ asset($author->author_image) }}" alt="Author Image"> -->
-    <p>{{ $author->author_name }}</p>
-</div>
-@endforeach
-
