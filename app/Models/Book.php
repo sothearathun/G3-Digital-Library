500836a76ba79a7e1e7f414f4695fa6c7d0373b3
kakadaaaaaaaaa <khuot@paragoniu.edu.kh>
2025-07-09 15:59:11 +0700

    Initial commit

---
+++ app/Models/Book.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+// use storage
+use Illuminate\Support\Facades\Storage;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $table = 'books';
+    protected $primaryKey = 'book_id';
+
+    protected $fillable = [
+        'book_title',
+        'description',
+        'prologue',
+        'total_pages',
+        'book_categories',
+        'author_name',
+        'released_date',
+        'book_genres',
+        'book_cover',
+        'file_path',
+        'file_format'
+    ];
+
+    protected $casts = [
+        'created_at',
+        'updated_at'
+    ];
+
+    // relationship
+    public function genres()
+    {
+        return $this->belongsToMany(Genres::class);
+    }
+    public function authors()
+    {
+        return $this->belongsToMany(Author::class);
+    }
+    public function comments()
+    {
+        return $this->hasMany(Book_Comments::class, 'book_id', 'book_id');
+    }
+
+    public function favorites()
+    {
+        return $this->hasMany(Book_Favorites::class, 'book_id', 'book_id');
+    }
+
+    public function ratings()
+    {
+        return $this->hasMany(Book_Ratings::class, 'book_id', 'book_id');
+    }
+
+    public function readingProgress()
+    {
+        return $this->hasMany(Reading_Progress::class, 'book_id', 'book_id');
+    }
+
+    
+    // public function comments() {
+    //     return $this->hasMany(Book_Comments::class);
+    // }
+
+    // public function ratings() {
+    //     return $this->hasMany(Book_Ratings::class);
+    // }
+
+    // public function favorites() {
+    //     return $this->hasMany(Book_Favorites::class);
+    // }
+
+    // public function readingProgress() {
+    //     return $this->hasMany(ReadingProgress::class);
+    // }
+}
---
+++ app/Models/Book_Comments.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book_Comments extends Model
+{
+    use HasFactory;
+
+    protected $table = 'book_comments';
+    protected $primaryKey = 'comment_id';
+    protected $fillable = ['comment_text', 'comment_title'];
+    protected $dates = ['created_at'];
+
+    public function book()
+    {
+        return $this->belongsToMany(Book::class);
+    }
+    public function user()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
